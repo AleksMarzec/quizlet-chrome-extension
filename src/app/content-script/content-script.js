b8c5f842483c521ba02614075131b6423bc99235
Aleksandra <aleks.marzec@gmail.com>
2019-09-14 21:00:47 +0200

    Communication between content script and background.

--- src/app/content-script/content-script.js
+++ src/app/content-script/content-script.js
@@ -1,4 +1,5 @@
 document.addEventListener("selectionchange", event => {
     let selection = document.getSelection ? document.getSelection().toString() : document.selection.createRange().toString();
     console.log(selection);
+    chrome.runtime.sendMessage( { message: "test123" });
 })
--- src/app/event-page/event-page.component.ts
+++ src/app/event-page/event-page.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    //this.initializeMessageListener();
+    this.initializeRequestListener();
+   }
 
   ngOnInit() {
   }
 
+  private initializeRequestListener() {
+    chrome.runtime.onMessage.addListener(function(request, sender) {
+      alert(`Received ${request}`);
+    });
+  }
+
+  private initializeMessageListener(): void {
+    chrome.runtime.onConnect.addListener(this.onConnectHandler.bind(this));
+  }
+
+  private onConnectHandler(port: chrome.runtime.Port) {
+    port.onMessage.addListener(this.onConnectMessageHandler.bind(this));
+  }
+
+  private onConnectMessageHandler(msg, port) {
+    console.log('Received connection message: ' + msg);
+    const response = 'Greetings!';
+    port.postMessage(response);
+  }
 }
