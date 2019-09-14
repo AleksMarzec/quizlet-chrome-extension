d9434c7f412df7d7954b1735a82baad49a0f2ba9
Aleksandra <aleks.marzec@gmail.com>
2019-09-14 20:18:29 +0200

    Add event listener for selection change.

--- src/app/content-script/content-script.js
+++ src/app/content-script/content-script.js
@@ -1,1 +1,4 @@
-console.log("test");
+document.addEventListener("selectionchange", event => {
+    let selection = document.getSelection ? document.getSelection().toString() : document.selection.createRange().toString();
+    console.log(selection);
+})
